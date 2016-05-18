dbba9b49ffed6aba1926a04d74be34f63f0306c1
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2016-05-18 00:03:34 -0500

    initial commit

---
+++ src/BreweryDb.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ethanclevenger91\BreweryDb;
+
+use GuzzleHttp\Client;
+
+class BreweryDb {
+  private $client;
+  private $baseUri = 'http://api.brewerydb.com/v2/';
+  private $key;
+
+  private $queryArgs;
+
+  public function getQueryArgs() {
+    return $this->queryArgs;
+  }
+  
+  public function setQueryArgs($args) {
+    $args['key'] = $this->key;
+    $this->queryArgs = $args;
+  }
+
+  public function __construct($key) {
+    $this->client = new Client([
+      'baseUr' => $this->baseUri,
+    ]);
+    $this->key = $key;
+  }
+
+  public function brewery($args, $method = 'GET') {
+    $this->updateArgs($args);
+    return $this->client->request($method, 'brewery', [
+      'query' => $this->queryArgs,
+    ]);
+  }
+}
---
+++ tests/BreweryDbTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ethanclevenger91\BreweryDb\Tests;
+
+use GuzzleHttp\Client;
+
+class BreweryDbTest extends PHPUnit_Framework_TestCase {
+
+  private $breweryDbInstance;
+  private $key = 'asdfqwer';
+
+  function __construct() {
+    parent::setUp();
+  }
+
+  function setUp() {
+    $this->breweryDbInstance = new BreweryDb($this->key);
+  }
+
+  public function testsetQueryArgsAddsKey() {
+    $this->breweryDbInstance->setQueryArgs([]);
+    $this->assertArrayHasKey('key', $this->breweryDbInstance->getQueryArgs());
+    $this->assertTrue($this->breweryDbInstance->getQueryArgs()['key'] == $this->key);
+  }
+
+}
